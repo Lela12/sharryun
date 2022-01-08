4f5712ed2c75bcb16f0350b0221c6ddf4248ae05
Lela12 <dbcocotn4@hanmail.net>
2022-01-08 22:22:40 +0900

    몽구스 DB연결

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# sharryun" 
 "# sharryun" 
 "# sharryun" 
+"# sharryun" 
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+//모듈에서 몽구스를 가져온다
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
